2d30a05868bf1cca91ed6852ac32adeb17ec92d0
Shisei Hanai <shanai@jp.ibm.com>
2022-12-15 04:45:32 +0000

    Add GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import { graphql, useStaticQuery } from 'gatsby';
-
 import Layout from '../components/Layout';
-
 import styles from './index.module.css';
+import BlogList from '../components/BlogList';
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
@@ -22,6 +21,7 @@ export default function IndexPage() {
       <div id={styles.hero}>
         <h1>{data.site.siteMetadata.title}</h1>
       </div>
+      <BlogList />
     </Layout>
   );
 }
